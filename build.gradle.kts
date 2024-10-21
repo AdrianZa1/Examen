66ad9ff0277d0db3c151eb6c44d5c043ebcd18b5
Kerly Anchundia <e1314863968@live.uleam.edu.ec>
2024-10-21 13:46:42 -0500

    correcion

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,7 @@
-import org.jetbrains.kotlin.gradle.internal.kapt.incremental.UnknownSnapshot.classpath
 
 buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath 'com.google.gms:google-services:4.3.15'
-        // Añade aquí otros classpath si los necesitas
+    extra.apply {
+        set("room_version", "2.6.0")
     }
 }
 
@@ -17,12 +11,6 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
